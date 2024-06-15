f5d32531469869218e4216ba695327f13c5b5a5b
Rohit Sidhwani <rhtsid18.dev@gmail.com>
2024-06-15 23:03:28 +0530

    Added UI Animate Plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,6 +11,8 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import any extra modules here
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
